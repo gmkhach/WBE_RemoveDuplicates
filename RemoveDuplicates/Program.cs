52361b0ab85db8b24fa95f50fb4f274e6174e96e
Gabriel Khachatryan <gmkhach@gmail.com>
2018-11-22 23:18:59 -0500

    brute-force method

--- RemoveDuplicates/Program.cs
+++ RemoveDuplicates/Program.cs
@@ -10,6 +10,40 @@ namespace RemoveDuplicates
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("Enter a sentence\n\n>>> ");
+                    string input = Console.ReadLine().Trim();
+                    Console.WriteLine($"\nResult: {RemoveDuplicates(input)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.Write("\nPress Enter to try another sentence...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        static string RemoveDuplicates(string input)
+        {
+            Queue<char> myQ = new Queue<char>();
+            foreach(char ch in input)
+            {
+                if (ch == ' ' || !myQ.Contains(ch))
+                {
+                    myQ.Enqueue(ch);
+                }
+            }
+            StringBuilder mySb = new StringBuilder();
+            foreach (char ch in myQ)
+            {
+                mySb.Append(ch);
+            }
+            return mySb.ToString();
         }
     }
 }
